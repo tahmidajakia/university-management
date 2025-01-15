fa393c60bcfcf2276acbf1bab03daba86c900fac
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2025-01-15 01:58:33 -0800

    Done Create Student Features

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 // parser
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 10;
-  res.send(a);
-});
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  res.send('Hello World');
+};
+
+app.get('/', getAController);
 
 export default app;
